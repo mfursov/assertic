f4ea6e62f8d0117480ac62e3787ea9583fc6f29b
Mikhail Fursov <mike.fursov@gmail.com>
2025-05-01 18:09:22 -0500

    Add ESM module to the dist

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+module.exports = [
+  {
+    entry: './src/index.ts',
+    target: 'node',
+    mode: 'production',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.cjs.js',
+      library: {
+        type: 'commonjs2',
+      },
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  },
+  {
+    entry: './src/index.ts',
+    target: 'node',
+    mode: 'production',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'index.esm.js',
+      library: {
+        type: 'module',
+      },
+    },
+    experiments: {
+      outputModule: true,
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+  },
+];
